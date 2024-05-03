f85b46fb93b4d6c4ca36710bc3a2e702e8be5b32
Maxim Podolsky <78932989+MVDem@users.noreply.github.com>
2024-05-03 18:15:49 +0300

    Update editFarmer.dto.ts
    added @IsOptoinal

--- src/farmers/dtos/editFarmer.dto.ts
+++ src/farmers/dtos/editFarmer.dto.ts
@@ -1,16 +1,24 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNumber, IsString, Length, Max } from 'class-validator';
+import {
+  IsEmail,
+  IsNumber,
+  IsString,
+  Length,
+  MaxLength,
+  IsOptional,
+} from 'class-validator';
 
-export class SignFarmerDto {
+export class EditFarmerDto {
   @ApiProperty({
     example: "Bob's ferma",
     description: 'Full name ',
   })
   @IsString({ message: 'Name must be a string' })
-  @Max(15, {
+  @MaxLength(15, {
     message: 'Name must be max 15 simbols ',
   })
-  readonly name?: string;
+  @IsOptional()
+  readonly name: string;
 
   @ApiProperty({
     example:
@@ -18,37 +26,41 @@ export class SignFarmerDto {
     description: 'Description must be a string and max 150 simbols',
   })
   @IsString({ message: 'Description must be a string' })
-  @Max(150, {
+  @MaxLength(150, {
     message: 'Description must be max 150 simbols',
   })
-  description?: string;
+  @IsOptional()
+  description: string;
 
   @ApiProperty({
     example: 'Qiryat Shemona',
     description: 'City name must be a string and max 15 simbols',
   })
   @IsString({ message: 'City name must be a string' })
-  @Max(15, {
+  @MaxLength(15, {
     message: 'Name must be max 15 simbols ',
   })
-  city?: string;
+  @IsOptional()
+  city: string;
 
   @ApiProperty({
     example: 'lorem ipsum dolor sit amet, consectetur adipiscing',
     description: 'Address must be a string and max 50 simbols',
   })
   @IsString({ message: 'Address must be a string' })
-  @Max(50, {
+  @MaxLength(50, {
     message: 'Address must be max 50 simbols',
   })
-  address?: string;
+  @IsOptional()
+  address: string;
 
   @ApiProperty({
     example: 'farmer@mail.ru',
     description: 'Email should be an email',
   })
   @IsEmail({}, { message: 'Email is not correct' })
-  email?: string;
+  @IsOptional()
+  email: string;
 
   @ApiProperty({
     example: '055 555 5555',
@@ -56,19 +68,22 @@ export class SignFarmerDto {
   })
   @IsNumber({}, { message: 'Phone is not a number' })
   @Length(10)
-  phone?: number;
+  @IsOptional()
+  phone: number;
 
   @ApiProperty({
     example: '739745.6585827',
     description: 'Coordinate latitude should be a number',
   })
   @IsNumber({}, { message: 'Coordinate latitude is not a number' })
-  coordinateLat?: number;
+  @IsOptional()
+  coordinateLat: number;
 
   @ApiProperty({
     example: '3677220.3427914',
     description: 'Coordinate longitude should be a number',
   })
   @IsNumber({}, { message: 'Coordinate longitude is not a number' })
-  coordinateLong?: number;
+  @IsOptional()
+  coordinateLong: number;
 }

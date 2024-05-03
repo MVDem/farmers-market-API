2d7d51045b86b54c4dc2a047cf307d245977db67
mvdem <m9602598255@gmail.com>
2024-05-03 18:22:42 +0300

    #5 create common farmer's method (CRUD)

--- src/farmers/farmers.service.ts
+++ src/farmers/farmers.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { SignFarmerDto } from './dtos/sign.dto';
+import { EditFarmerDto } from './dtos/editFarmer.dto';
 
 @Injectable()
-export class FarmersService {}
+export class FarmersService {
+  constructor() {}
+
+  async signIn(dto: SignFarmerDto) {
+    const farmer = {
+      login: dto.login,
+      password: dto.password,
+    };
+    console.log('Sign in as a farmer:', farmer);
+    return farmer;
+  }
+
+  async signUp(dto: SignFarmerDto) {
+    const farmer = {
+      login: dto.login,
+      password: dto.password,
+    };
+    console.log('Sign in as a farmer:', farmer);
+    return farmer;
+  }
+
+  async updateFarmer(dto: EditFarmerDto) {
+    Object.entries(dto).map(([key, value]) => {
+      console.log(`Update farmer ${key}:`, value);
+    });
+    return dto;
+  }
+
+  async getOne(id: string) {
+    console.log('Get farmer by id:', id);
+    return id;
+  }
+
+  async deleteFarmer(id: string) {
+    console.log('Delete farmer by id:', id);
+    return id;
+  }
+}

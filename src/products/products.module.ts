2862f165a4b0dacaa621581bfad281bd51b70739
vladoszhar <vladoszhar@gmail.com>
2024-06-06 20:30:53 +0300

    Create dependency between offer and product

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,16 @@ import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { Product } from './products.model';
+import { Offer } from 'src/offers/offers.model';
+import { Farmer } from 'src/farmers/farmers.model';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [SequelizeModule.forFeature([Product])],
+
+  imports: [
+    SequelizeModule.forFeature([Product, Offer, Farmer])
+  ],
+  exports: [ProductsService],
 })
 export class ProductsModule {}

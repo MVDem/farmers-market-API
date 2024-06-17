1c8623649a518cd2f42cf408945d4d8fbb0b00e2
AndreevSV <andreevsv@gmail.com>
2024-06-17 21:54:20 +0300

    chore: Update database configuration for staging environment

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,18 +21,49 @@ import { CategoriesModule } from './categories/categories.module';
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
     }),
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      models: [Auth, Farmer, Offer, Product, Category],
-      autoLoadModels: true,
-      synchronize: true,
-      logging: false,
+    SequelizeModule.forRootAsync({
+      useFactory: () => {
+        let options: Record<string, unknown> = {};
+
+        if (process.env.NODE_ENV === 'development') {
+          options = {
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [Auth, Farmer, Offer, Product, Category],
+            autoLoadModels: true,
+            synchronize: true,
+            logging: false,
+          };
+        } else {
+          options = {
+            dialect: 'postgres',
+            models: [Auth, Farmer, Offer, Product, Category],
+            autoLoadModels: true,
+            synchronize: true,
+            logging: false,
+            uri: process.env.POSTGRES_URI,
+          };
+        }
+
+        return options;
+      },
     }),
+    // SequelizeModule.forRoot({
+    //   dialect: 'postgres',
+    //   host: process.env.POSTGRES_HOST,
+    //   port: Number(process.env.POSTGRES_PORT),
+    //   username: process.env.POSTGRES_USER,
+    //   password: process.env.POSTGRES_PASSWORD,
+    //   database: process.env.POSTGRES_DB,
+    //   models: [Auth, Farmer, Offer, Product, Category],
+    //   autoLoadModels: true,
+    //   synchronize: true,
+    //   logging: false,
+    // }),
     AuthModule,
     FarmersModule,
     ProductsModule,
--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
-  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
+  await app.listen(PORT, () => {
+    console.log(process.env.NODE_ENV)
+    console.log(`Server started on port = ${PORT}`)})
 }
 bootstrap();
